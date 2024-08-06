45c184955b498410a77e426922d662ec34a600f1
Mohammad Nazir <nazir2122@gmail.com>
2024-08-06 13:07:35 +0530

    Added serialVersionUID in model class

--- user-producer/src/main/java/com/kafka/user/model/UserRequest.java
+++ user-producer/src/main/java/com/kafka/user/model/UserRequest.java
@@ -6,6 +6,7 @@ import java.io.Serializable;
 
 @Data
 public class UserRequest implements Serializable {
-    public UserInfo userInfo;
+    private static final long serialVersionUID = 6624075166255122029L;
+	public UserInfo userInfo;
     public UserResponse userResponse;
 }
--- user-producer/src/main/java/com/kafka/user/model/UserResponse.java
+++ user-producer/src/main/java/com/kafka/user/model/UserResponse.java
@@ -7,7 +7,8 @@ import java.time.LocalDate;
 
 @Data
 public class UserResponse implements Serializable {
-    public String eventStatus;  // success, pending, fail
+    private static final long serialVersionUID = 7290554912560684845L;
+	public String eventStatus;  // success, pending, fail
     public String userName;  // first + middle + last name
     public String uniqueEventId; //userid+lastName+pincode+aadhar+currentDate;
     public LocalDate eventTime;
