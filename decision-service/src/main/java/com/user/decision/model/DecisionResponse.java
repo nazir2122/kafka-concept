c0bf3705156aed881921cafea6f6cd570b66fb04
Mohammad Nazir <nazir2122@gmail.com>
2024-08-16 16:06:58 +0530

    minor changes

--- decision-service/src/main/java/com/user/decision/model/DecisionResponse.java
+++ decision-service/src/main/java/com/user/decision/model/DecisionResponse.java
@@ -10,5 +10,5 @@ public class DecisionResponse implements Serializable {
     public String responseCode; //0000-success,0001-failed,0002-alerts
     public String decisionExecutionDate;
     public String routingIndicator;
-
+    public String decisionStatus="decisionStatus";
 }
